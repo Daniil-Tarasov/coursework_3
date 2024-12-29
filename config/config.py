01196d74ade437ba212507f71396bc1fdf3b66ea
Daniil Tarasov <danya.tarasov.01@mail.ru>
2024-12-29 23:25:43 +0300

    continue with user interaction

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ from configparser import ConfigParser
 
 def config(
     filename="/Users/danya/my_prj/coursework_3/config/database.ini",
-    section="postgres",
+    section="postgresql",
 ):
     # create a parser
     parser = ConfigParser(interpolation=None)
--- src/user_interaction.py
+++ src/user_interaction.py
@@ -1,5 +1,7 @@
 from config.config import config
 from src.hh_api import HHApi
+from src.utils import get_data_employer_by_id
+from src.vacancies import Vacancies
 
 
 def user_interaction():
@@ -10,14 +12,24 @@ def user_interaction():
     default_name = "employers"
 
     params = config()
-    hh = HHApi()
 
     print("Здравствуйте, пользователь!")
     while answer.lower() != "n":
+        hh = HHApi()
         print("Яндекс\nТ-банк\nGoogle\nСБЕР\nSkypro\nХ5 Group\nVK\nApple\nMicrosoft\nNVIDIA")
         query = input("Введите название компании по которой желаете получить данные или скопируйте из списка сверху: ")
         if query:
             employers = hh.load_employers(query) # поиск работодателей в hh по запросу пользователя
+            print(employers)
+            hh = HHApi()
+            user_id = input("Вставьте ID компании, для поиска вакансий ")
+            user_employer = get_data_employer_by_id(employers, user_id) # получаем нужную компанию
+            user_employers.extend(user_employer)
+            print(user_employers)
+            vacancies = hh.load_vacancies_by_id(user_id) # получаем список вакансий по ID компании
+            print(vacancies)
+            vacancies = Vacancies.get_vacancies_from_list(vacancies) # записываем вакансии в класс Vacancies
+            print(vacancies)
         else:
             print("Не оставляйте поле пустым")
         answer = input("Желаете выполнить поиск ещё раз? y/n ")
